b029abfe19f25063ccb6fd73add96d344420e9fd
PEDRO AKSARLIAN <49554455@ort>
2026-06-03 08:20:46 -0300

    Creacion del proyecto y avance en clase PalabrasAhorcado

---
+++ Models/PalabrasAhorcado.cs
@@ -0,0 +1,20 @@
+class PalabrasAhorcado{
+
+private List <string> palabras;
+
+public PalabrasAhorcado(){
+    this.palabras = new List <string>;
+    this.palabras.Add() = ("AVENTURA");
+    this.palabras.Add() = ("ELEFANTE");
+    this.palabras.Add() = ("ORQUESTA");
+    this.palabras.Add() = ("GUITARRA");
+    this.palabras.Add() = ("MARIPOSA");
+    this.palabras.Add() = ("CHOCOLATE");
+    this.palabras.Add() = ("PRIMAVERA");
+    this.palabras.Add() = ("ASTRONAUTA");
+    this.palabras.Add() = ("COMPUTADORA");
+    this.palabras.Add() = ("ELECTRODOMESTICO");
+}
+
+
+}
