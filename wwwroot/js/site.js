7404965f907e6736d7e1eeaf3f634446f43b983b
PEDRO AKSARLIAN <49554455@ort>
2026-06-04 09:03:53 -0300

    Avance en el js y html

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿const palabraCambiar = document.getElementById("palabra");
+﻿const h2 = document.getElementById("palabra");
+const palabraCambiar = document.getElementById("palabraAdivinar").value;
+let acumulador = "";
+let intentos = 10; 
+
+for(let i = 0; i < palabraCambiar.length; i++){
+    acumulador += "_ ";
+}
+h2.innerHTML = acumulador;
+
+function ArriesgarLetra(){
+    const letra = document.getElementById("letra").value;
+    letra = letra.toUpperCase();
+    for(let i = 0;  i < palabraCambiar.length; i++){
+        if(palabraCambiar[i]=== letra){
+             
+    }
+    }
+}   
