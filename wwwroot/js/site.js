958a5da1ad10aee105e64fc980d81ef8d835742e
PEDRO AKSARLIAN <49554455@ort>
2026-06-10 09:08:36 -0300

    Continuamos el JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,19 +1,43 @@
-﻿const h2 = document.getElementById("palabra");
+﻿let h2 = document.getElementById("palabra");
+let h3 = document.getElementById("intentos");
 const palabraCambiar = document.getElementById("palabraAdivinar").value;
+let mensaje = document.getElementById("mensaje");
 let acumulador = "";
 let intentos = 10; 
+let intermedio = "";
+
+console.log(palabraCambiar);
 
 for(let i = 0; i < palabraCambiar.length; i++){
     acumulador += "_ ";
 }
 h2.innerHTML = acumulador;
+h3.innerHTML = "Te quedan " + intentos + " intentos";
+
+intermedio = acumulador;
 
 function ArriesgarLetra(){
-    const letra = document.getElementById("letra").value;
+    let letra = document.getElementById("letra").value;
     letra = letra.toUpperCase();
     for(let i = 0;  i < palabraCambiar.length; i++){
-        if(palabraCambiar[i]=== letra){
-             
+        if(palabraCambiar[i] === letra){
+            intermedio[i] = letra;
+            h2.innerHTML = intermedio;
+    }else{
+        intentos -= 1;
+        h3.innerHTML = "Te quedan " + intentos + " intentos";
     }
     }
-}   
+}
+
+if(intermedio === palabraCambiar)
+{
+    mensaje.innerHTML = "Ganaste!!";
+}
+else if(intentos == 0)
+    {
+        mensaje.innerHTML = "Has perdido";
+    }
+    else{
+        
+    }
